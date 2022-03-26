17ac4cdf1fdae773749e3e5b4abe852518761f5d
robertomatheus <as.matheusroberto@gmail.com>
2022-03-26 16:52:24 -0300

    Adicionando filtros para pegar tipo e index

--- gestao_competencia/templatetags/custom_tags.py
+++ gestao_competencia/templatetags/custom_tags.py
@@ -2,8 +2,16 @@ from django import template
 
 register = template.Library()
 
+@register.filter
 @register.simple_tag
 def update_value(value):
     return value
+
+@register.filter
+def get_type(value):
+    return type(value)
+
+@register.filter
+def index(indexable, i):
+    return indexable[int(i)]
     
-register.filter('update_value', update_value)
