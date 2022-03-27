5106d508ac27cbe1a438cc9cf5bcd7c07b2883d6
robertomatheus <as.matheusroberto@gmail.com>
2022-03-27 13:04:24 -0300

    Adicionando barra de busca em django.admin

--- gestao_competencia/admin.py
+++ gestao_competencia/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Area)
-admin.site.register(Colaborador)
-admin.site.register(SoftSkill)
-admin.site.register(HardSkill)
+class NameId(admin.ModelAdmin):
+    search_fields = ['id', 'nome']
+
+admin.site.register(Area, NameId)
+admin.site.register(Colaborador, NameId)
+admin.site.register(SoftSkill, NameId)
+admin.site.register(HardSkill, NameId)
 admin.site.register(ColaboradorHardSkill)
 admin.site.register(ColaboradorSoftSkill)
+admin.site.register(Startup, NameId)
+admin.site.register(SubArea, NameId)
+admin.site.register(Escala)
