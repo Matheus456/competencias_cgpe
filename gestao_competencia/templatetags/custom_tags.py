4be439d14ec16db970e25deae4e00fe9c3a2a5aa
robertomatheus <as.matheusroberto@gmail.com>
2022-03-27 13:01:44 -0300

    Adicionando filtro para retornar escala

--- gestao_competencia/templatetags/custom_tags.py
+++ gestao_competencia/templatetags/custom_tags.py
@@ -15,3 +15,10 @@ def get_type(value):
 def index(indexable, i):
     return indexable[int(i)]
     
+@register.filter
+def return_scale(skill_obj):
+    if skill_obj.escala:
+        scale = skill_obj.escala
+        return [scale.legenda_1, scale.legenda_2, scale.legenda_3, scale.legenda_4, scale.legenda_5]
+    else:
+        return ["Nenhuma Experiência", "", "Alguma Experiência", "", "Bastante Experiência"]
