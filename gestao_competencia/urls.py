7755919940af6ea819d0f63d2b6f56ee4c35ee7e
robertomatheus <as.matheusroberto@gmail.com>
2022-03-30 17:19:55 -0300

    Adicionando rota da dashboard

--- gestao_competencia/urls.py
+++ gestao_competencia/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.encontrar_usuario, name='encontrar_usuario'),
     path('<str:id>/formulario_competencias', views.formulario_competencias, name='formulario_competencias'),
     path('<str:id>/avaliar_competencias', views.avaliar_compentecias, name='avaliar_compentecias'),
+    path('dashboard/startup', views.dashboard_startup, name='dashboard_startup'),
 ]
